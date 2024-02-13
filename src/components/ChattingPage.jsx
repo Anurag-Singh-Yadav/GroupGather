34a9bd0a9be643658e0db6d4cce65e233821d76d
Anurag_Singh_Yadav <anuragsinghyadav0005@gmail.com>
2024-02-13 23:41:02 +0530

    meassage with use name

--- src/components/ChattingPage.jsx
+++ src/components/ChattingPage.jsx
@@ -2,25 +2,35 @@
 
 import React from "react";
 
-const ChattingPage = ({ inputMessage, setInputMessages, sendMessage, allMessages }) => {
+const ChattingPage = ({
+  inputMessage,
+  setInputMessages,
+  sendMessage,
+  allMessages,
+}) => {
+  console.log("ChattingPage",allMessages);
   return (
-    <div>
+    <div className="relative h-[100vh] bg-green-300">
       {/* Render all messages */}
-      {allMessages.map((message, index) => (
-        <div key={index}>
-          <span>{message.userName}: </span>
-          <span>{message.message}</span>
-        </div>
-      ))}
-      
+      <div>
+        {allMessages.map((message, index) => (
+          <div className="text-black" key={index}>
+            <span>{message.userName}: </span>
+            <span>{message.message}</span>
+          </div>
+        ))}
+      </div>
+
       {/* Input field for sending new messages */}
-      <input 
-        type="text" 
-        className="text-black"
-        value={inputMessage} 
-        onChange={(e) => setInputMessages(e.target.value)} 
-      />
-      <button onClick={sendMessage}>Send</button>
+      <div className="absolute bottom-1 flex w-full">
+        <input
+          type="text"
+          className="text-black w-full py-2 px-4"
+          value={inputMessage}
+          onChange={(e) => setInputMessages(e.target.value)}
+        />
+        <div className="px-2 py-2 cursor-pointer bg-red-500" onClick={()=>{sendMessage()}}>Send</div>
+      </div>
     </div>
   );
 };
